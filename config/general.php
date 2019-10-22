71ced8ddf596411c5c1e47ee01fa62b64cecd54e
Jan Henckens <jan@statik.be>
2019-10-22 16:15:43 +0200

    Hide GQL

--- config/general.php
+++ config/general.php
@@ -10,6 +10,7 @@ return [
     // Global settings
     '*' => [
         'useProjectConfigFile' => getenv("PROJECT_CONFIG") ?? false,
+        'enableGql' => false,
         'defaultWeekStartDay' => 1,
         'useEmailAsUsername' => true,
         'enableCsrfProtection' => true,
