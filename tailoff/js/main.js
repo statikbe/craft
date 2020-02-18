2471e0845321125c64ae35807447776f0eb46d0a
Karel-Jan Van Haute <karel-jan@statik.be>
2020-02-18 12:29:20 +0100

    no message

--- tailoff/js/main.js
+++ tailoff/js/main.js
@@ -6,12 +6,10 @@ import "../icons/iconfont.font";
 import "../css/main.css";
 
 import Vue from "vue";
-import vuetify from "./plugins/vuetify";
 import Test from "./components/Test";
 import { Toggle } from "./directives/Toggle";
 
 new Vue({
-    vuetify,
     el: "#app",
     components: {
         "test-component": Test
