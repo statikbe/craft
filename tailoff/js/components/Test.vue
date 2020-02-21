bee9537d3f8769e68f87931b2475f5e13663f821
Karel-Jan Van Haute <karel-jan@statik.be>
2020-02-21 11:43:37 +0100

    vue test

---
+++ tailoff/js/components/Test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>This is the message: {{ msg }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+};
+</script>
---
+++ tailoff/js/components/ValidationWrapper.vue
@@ -0,0 +1,37 @@
+<template>
+  <form class="wrapper">
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  bind(el) {
+    console.log(el);
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+    log: function() {
+      console.log("This is it!");
+    }
+  }
+};
+// import { ValidationProvider, extend } from "vee-validate";
+// import * as rules from "vee-validate/dist/rules";
+
+// Object.keys(rules).forEach(rule => {
+//   extend(rule, rules[rule]);
+// });
+
+// export default {
+//   inheritAttrs: false,
+//   components: {
+//     ValidationProvider
+//   },
+//   props: {
+//     msg: String
+//   }
+// };
+</script>
