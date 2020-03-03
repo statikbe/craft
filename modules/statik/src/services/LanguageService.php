faaee79639258a5c16ede84b18bd5e00879bc99d
Jan Henckens <jan@statik.be>
2020-03-03 10:27:22 +0100

    Updated language redirect function & added doc blocks

--- modules/statik/src/services/LanguageService.php
+++ modules/statik/src/services/LanguageService.php
@@ -10,11 +10,19 @@ use modules\statik\Statik;
 class LanguageService extends Component
 {
 
+    /**
+     * Here we'll determine if (and where to) the user should be redirected based on
+     * - where they enter the site
+     * - if they already have a lanugage cooie
+     * - which language their browser is set to
+     * - which languages we have in the site
+     * @throws \craft\errors\SiteNotFoundException
+     * @throws \yii\base\ExitException
+     */
     public function redirect()
     {
         $cookie = Statik::LANGUAGE_COOKIE;
         $hasCookie = isset($_COOKIE[$cookie]) ?? false;
-
 
         if (!$hasCookie && !isset($_GET['p'])) {
             Craft::debug('No language cookie found and on root, settings language cookie & redirecting', 'Statik');
@@ -34,7 +42,9 @@ class LanguageService extends Component
         }
     }
 
-
+    /**
+     * This function will get the site we need to redirect to base on the Statik::LANGUAGE_COOKIE cookie
+     */
     public function redirectLanguage()
     {
         $siteHandle = $_COOKIE[Statik::LANGUAGE_COOKIE];
@@ -42,12 +52,22 @@ class LanguageService extends Component
         $this->redirectToSite($site);
     }
 
+    /**
+     * This funcion only runs when the user enters the site on the root and doesn't have a cookie yet
+     * It will look at the language available in the current install, and the languages accepted by the browser.
+     * - If a match can be made, we'll set the Statik::LANGUAGE_COOKIE cookie to that site handle and redirect the user
+     * - If no match can be made, we'll redirect to the primary site and set a cookie for that site.
+     *
+     * @throws \yii\base\ExitException
+     */
     public function detectLanguage()
     {
         $sites = Craft::$app->getSites()->getAllSites();
         $availableLanguages = [];
         foreach ($sites as $site) {
-            $availableLanguages[$site->id] = $site->language;
+            $availableLanguages[$site->language] = (int)$site->id;
+            $language = explode('-', $site->language);
+            $availableLanguages[$language[0]] = (int)$site->id;
         }
 
         $acceptedLanguages = [];
@@ -56,15 +76,15 @@ class LanguageService extends Component
             $acceptedLanguages[] = $lang[0];
         }
 
-        $matchedLocales = array_intersect($acceptedLanguages, $availableLanguages);
+        $matchedLocales = array_intersect($acceptedLanguages, array_keys($availableLanguages));
         $sortedSites = [];
-        foreach ($matchedLocales as $locale) {
-            $key = array_keys($availableLanguages, $locale);
-            $sortedSites[$key[0]] = $locale;
+
+        foreach($matchedLocales as $locale) {
+            $sortedSites[$locale] = $availableLanguages[$locale];
         }
 
         if ($sortedSites) {
-            $siteId = array_key_first($sortedSites);
+            $siteId = reset($sortedSites);
             /** @var Site $site */
             $site = Craft::$app->getSites()->getSiteById($siteId);
             $this->setLanguageCookie($site);
@@ -72,12 +92,22 @@ class LanguageService extends Component
         }
     }
 
+    /**
+     * This function will redirect the user to the site's baseUrl
+     * @param $site
+     * @throws \yii\base\ExitException
+     */
     public function redirectToSite($site)
     {
         Craft::$app->getResponse()->redirect($site->baseUrl);
         Craft::$app->end();
     }
 
+
+    /**
+     * The function will set the Statik::LANGUAGE_COOKIE cookie to the handle of the site that is passed to it
+     * @param Site $site
+     */
     public function setLanguageCookie(Site $site)
     {
         $handle = $site->handle;
