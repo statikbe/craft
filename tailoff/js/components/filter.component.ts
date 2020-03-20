ca5ccf9f39e6a8f7bc838d7ab19f3d41bc46ca18
Karel-Jan Van Haute <karel-jan@statik.be>
2020-03-20 11:58:30 +0100

    Filter

--- tailoff/js/components/filter.component.ts
+++ tailoff/js/components/filter.component.ts
@@ -1,30 +1,44 @@
 import { ArrayPrototypes } from "../utils/prototypes/array.prototypes";
+import { FormPrototypes } from "../utils/prototypes/form.prototypes";
+import { ElementPrototype } from "../utils/prototypes/element.prototypes";
+import { ScrollHelper } from "../utils/scroll";
 
+ElementPrototype.activateMatches();
 ArrayPrototypes.activateFrom();
+FormPrototypes.activateSerialize();
 
 export class FilterComponent {
   private options = {
-    scrollToTopOfResults: true,
+    scrollToTopOfResults: false,
     disableScrollOnMobile: true
   };
 
-  private formElement: HTMLFormElement;
-  private submitButtonElement: HTMLButtonElement;
-  private filterChangeElements: Array<HTMLElement>;
-  private loaderAnimationElement: HTMLElement;
-  private resultsElement: HTMLElement;
-  private filterMobileToggleButtonElement: HTMLElement;
-  private filterMobileCollapseElement: HTMLElement;
-  private clearFilterButtonElement: HTMLElement;
-  private scrollToElement: HTMLElement;
-  private showMoreOptionElements: Array<HTMLElement>;
+  private formElement: HTMLFormElement; // .js-filter-form
+  private submitButtonElement: HTMLButtonElement; // <button type="submit"></button>
+  private filterChangeElements: Array<HTMLElement>; // input and select
+  private loaderAnimationElement: HTMLElement; // .js-filter-loader
+  private ariaLiveElement: HTMLElement; // .js-filter-aria-live
+  private resultsElement: HTMLElement; // .js-filter-results
+  private filterMobileToggleButtonElement: HTMLElement; // .js-filter-mobile-toggle
+  private filterMobileCollapseElement: HTMLElement; // .js-filter-mobile-collapse
+  private clearFilterButtonElement: HTMLElement; // .js-filter-clear
+  private scrollToElement: HTMLElement; // .js-filter-scroll-position
+  private showMoreOptionElements: Array<HTMLElement>; // .js-filter-show-more
+  private paginationElement: HTMLElement; // .js-filter-pagination
 
-  constructor(options: Object) {
+  private xhr: XMLHttpRequest;
+  private screenWidth = window.innerWidth;
+  private mobileBreakpoint = 820;
+  private scrollSpeed = 500;
+
+  constructor(options: Object = {}) {
     this.options = { ...this.options, ...options };
 
     this.formElement = document.querySelector(".js-filter-form");
+
     if (this.formElement) {
       this.resultsElement = document.querySelector(".js-filter-results");
+
       if (!this.resultsElement) {
         console.log(
           "You must have an element with class 'js-filter-results' defined in order for the filter plugin to work."
@@ -32,9 +46,18 @@ export class FilterComponent {
         return;
       }
 
+      this.ariaLiveElement = document.querySelector(".js-filter-aria-live");
+      if (!this.ariaLiveElement) {
+        console.log(
+          "You must have an element with class 'js-filter-aria-live' defined in order for the filter plugin to work."
+        );
+        return;
+      }
+
       this.submitButtonElement = this.formElement.querySelector(
-        ".js-filter-submit"
+        "button[type=submit]"
       );
+
       if (this.submitButtonElement) {
         this.initSubmitButton();
       }
@@ -76,12 +99,237 @@ export class FilterComponent {
       this.initClearFilter();
     }
 
+    this.paginationElement = document.querySelector(".js-filter-pagination");
+    if (this.paginationElement) {
+      this.initPagination();
+    }
+
     this.scrollToElement = document.querySelector(".js-filter-scroll-position");
   }
-  private initSubmitButton() {}
-  private initFilterChangeElements() {}
-  private initLoader() {}
-  private initFilterToggle() {}
-  private initClearFilter() {}
-  private initShowMore() {}
+
+  private initSubmitButton() {
+    this.submitButtonElement.addEventListener("click", event => {
+      event.preventDefault();
+      this.getFormAction();
+    });
+  }
+
+  private initFilterChangeElements() {
+    this.filterChangeElements.forEach(el => {
+      el.addEventListener("change", () => {
+        this.getFormAction();
+      });
+    });
+  }
+
+  private initLoader() {
+    if (!this.loaderAnimationElement.classList.contains("hidden")) {
+      this.loaderAnimationElement.classList.add("hidden");
+    }
+  }
+  private initFilterToggle() {
+    this.filterMobileToggleButtonElement.setAttribute("role", "button");
+    this.filterMobileToggleButtonElement.classList.add("open");
+    this.filterMobileToggleButtonElement.setAttribute("aria-expanded", "true");
+    this.filterMobileToggleButtonElement.setAttribute(
+      "aria-controls",
+      "filterMobileCollapseArea"
+    );
+
+    this.filterMobileCollapseElement.setAttribute(
+      "id",
+      "filterMobileCollapseArea"
+    );
+    this.filterMobileCollapseElement.setAttribute("role", "region");
+
+    window.addEventListener("resize", () => {
+      if (window.innerWidth !== this.screenWidth) {
+        this.screenWidth = window.innerWidth;
+
+        this.openFilterMobileToggle(this.screenWidth > this.mobileBreakpoint);
+      }
+    });
+
+    this.filterMobileToggleButtonElement.addEventListener("click", e => {
+      e.preventDefault();
+      this.openFilterMobileToggle(
+        this.filterMobileCollapseElement.classList.contains("hidden")
+      );
+    });
+  }
+
+  private openFilterMobileToggle(open: boolean) {
+    if (open) {
+      this.filterMobileCollapseElement.classList.remove("hidden");
+      this.filterMobileToggleButtonElement.classList.add("open");
+      this.filterMobileToggleButtonElement.setAttribute(
+        "aria-expanded",
+        "true"
+      );
+    } else {
+      this.filterMobileCollapseElement.classList.add("hidden");
+      this.filterMobileToggleButtonElement.classList.remove("open");
+      this.filterMobileToggleButtonElement.setAttribute(
+        "aria-expanded",
+        "false"
+      );
+    }
+  }
+
+  private initClearFilter() {
+    this.clearFilterButtonElement.setAttribute("role", "button");
+    this.clearFilterButtonElement.addEventListener("click", e => {
+      e.preventDefault();
+      this.clearForm();
+      this.getFilterData("./", true);
+    });
+  }
+
+  private initShowMore() {
+    this.showMoreOptionElements.forEach(el => {
+      el.querySelector("a").addEventListener("click", e => {
+        e.preventDefault();
+        Array.from(
+          el.parentElement.querySelectorAll(".js-filter-extra-content")
+        ).forEach(extra => {
+          extra.classList.remove("hidden");
+        });
+        el.parentNode.removeChild(el);
+      });
+    });
+  }
+
+  private initPagination() {
+    document.addEventListener(
+      "click",
+      e => {
+        // loop parent nodes from the target to the delegation node
+        for (
+          let target = <Element>e.target;
+          target && !target.isSameNode(document);
+          target = target.parentElement
+        ) {
+          if (target.matches(".js-filter-pagination a")) {
+            e.preventDefault();
+            this.showLoading();
+            this.getFilterData((target as HTMLAnchorElement).href);
+            break;
+          }
+        }
+      },
+      false
+    );
+  }
+
+  private getFilterData(url, clearPage = false) {
+    const _self = this;
+    if (this.xhr) {
+      this.xhr.abort();
+    }
+
+    // Go back to page 1 when set changes
+    if (clearPage) {
+      const regexResult = window.location.pathname.match(
+        /([^\?\s]+\/)([p\d].?)(.*)/
+      );
+
+      if (regexResult && regexResult[1]) {
+        url = regexResult[1] + url;
+      }
+    }
+
+    this.xhr = new XMLHttpRequest();
+    this.xhr.open("GET", url, true);
+
+    this.xhr.onload = function() {
+      if (this.status >= 200 && this.status < 400) {
+        const responseElement = document.implementation.createHTMLDocument("");
+        responseElement.body.innerHTML = this.response;
+        _self.resultsElement.innerHTML = responseElement.querySelector(
+          ".js-filter-results"
+        ).innerHTML;
+
+        _self.ariaLiveElement.innerHTML = responseElement.querySelector(
+          ".js-filter-aria-live"
+        ).innerHTML;
+
+        history.pushState("", "New URL: " + url, url);
+        _self.hideLoading();
+
+        if (_self.options.scrollToTopOfResults && _self.scrollToElement) {
+          if (window.innerWidth < _self.mobileBreakpoint) {
+            if (!_self.options.disableScrollOnMobile) {
+              ScrollHelper.scrollToY(_self.scrollToElement, _self.scrollSpeed);
+            }
+          } else {
+            ScrollHelper.scrollToY(_self.scrollToElement, _self.scrollSpeed);
+          }
+        }
+      } else {
+        console.log("Something went wrong when fetching data");
+      }
+    };
+
+    this.xhr.onerror = function() {
+      console.log("There was a connection error");
+    };
+
+    this.xhr.send();
+  }
+
+  private getFormAction() {
+    this.showLoading();
+    const url =
+      this.formElement.getAttribute("action") +
+      "?" +
+      this.formElement.serialize();
+    this.getFilterData(url, true);
+  }
+
+  private showLoading() {
+    if (this.loaderAnimationElement) {
+      this.loaderAnimationElement.classList.remove("hidden");
+      this.resultsElement.classList.add("hidden");
+    }
+  }
+
+  private hideLoading() {
+    if (this.loaderAnimationElement) {
+      this.loaderAnimationElement.classList.add("hidden");
+      this.resultsElement.classList.remove("hidden");
+    }
+  }
+
+  private clearForm() {
+    this.formElement.reset();
+    const elements = Array.from(this.formElement.elements);
+
+    elements.forEach(el => {
+      const type = el.getAttribute("type").toLowerCase();
+
+      switch (type) {
+        case "text":
+        case "password":
+        case "textarea":
+        case "hidden":
+          el.setAttribute("value", "");
+          break;
+
+        case "radio":
+        case "checkbox":
+          if ((el as HTMLInputElement).checked) {
+            (el as HTMLInputElement).checked = false;
+          }
+          break;
+
+        case "select-one":
+        case "select-multi":
+          el.setAttribute("selectedIndex", "-1");
+          break;
+
+        default:
+          break;
+      }
+    });
+  }
 }
