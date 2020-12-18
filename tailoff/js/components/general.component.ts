25002403621dcf08f13f634994337d3c7efa015a
Karel-Jan Van Haute <karel-jan@statik.be>
2020-12-18 17:33:13 +0100

    svg icons

--- tailoff/js/components/general.component.ts
+++ tailoff/js/components/general.component.ts
@@ -1,6 +1,13 @@
+import svg4everybody from "svg4everybody";
 export class GeneralComponent {
   constructor() {
     this.addOutlineForTabbers();
+    this.initSvg4everybody();
+  }
+
+  // This can be removed if we ditch IE11
+  private initSvg4everybody() {
+    svg4everybody();
   }
 
   // This adds a class if the user is tabbing and thus using the keyboard, so the focus style will be visible. Otherwise if it's a clicker the focus is removed.
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ const PurgecssPlugin = require("purgecss-webpack-plugin");
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const SVGSpritemapPlugin = require("svg-spritemap-webpack-plugin");
 // const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 const PATHS = {
@@ -23,6 +24,7 @@ const PATHS = {
   templates: path.join(__dirname, "templates"),
   modules: path.join(__dirname, "modules"),
   tailoff: path.join(__dirname, "tailoff", "/js"),
+  icons: path.join(__dirname, "tailoff", "/icons"),
   ejs: path.join(__dirname, "tailoff", "/ejs"),
 };
 
@@ -134,6 +136,18 @@ module.exports = (env) => {
       ]),
       new ImageminPlugin({
         test: /\.img\.(jpe?g|png|gif)$/i,
+      }),
+      new SVGSpritemapPlugin(`${PATHS.icons}/**/*.svg`, {
+        output: {
+          filename: "icon/sprite.svg",
+        },
+        sprite: {
+          prefix: false,
+          generate: {
+            use: true,
+            view: "-icon",
+          },
+        },
       }),
       new Dotenv(),
       ...(!isDevelopment || env.purge
