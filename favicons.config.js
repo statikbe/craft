957895dd1c1c28ccadc6830c0b770830e7fb063b
Karel-Jan Van Haute <karel-jan@statik.be>
2021-06-09 12:29:20 +0200

    favicon build fix

--- favicons.config.js
+++ favicons.config.js
@@ -1,27 +1,27 @@
-const path = require("path");
+const path = require('path');
 
-const tailwindConf = require("./tailwind.config.js");
-const dotenv = require("dotenv").config({ path: __dirname + "/.env" });
+const tailwindConf = require('./tailwind.config.js');
+const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
 
 //  Plugins
-const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
-  public: path.join(__dirname, "public"),
-  templates: path.join(__dirname, "templates"),
-  favicon: path.join(__dirname, "tailoff", "/img"),
-  ejs: path.join(__dirname, "tailoff", "/ejs"),
+  public: path.join(__dirname, 'public'),
+  templates: path.join(__dirname, 'templates'),
+  favicon: path.join(__dirname, 'tailoff', '/img'),
+  ejs: path.join(__dirname, 'tailoff', '/ejs'),
 };
 
 module.exports = (env) => {
   return {
-    mode: "production",
+    mode: 'production',
     entry: {},
     output: {
-      publicPath: "/",
+      publicPath: '/',
       path: `${PATHS.public}`,
-      filename: "assets/favicon.js",
+      filename: 'assets/favicon.js',
     },
     plugins: [
       new HtmlWebpackPlugin({
@@ -34,12 +34,12 @@ module.exports = (env) => {
       }),
       new FaviconsWebpackPlugin({
         logo: `${PATHS.favicon}/favicon.svg`,
-        devMode: "webapp",
+        devMode: 'webapp',
         cache: true,
         favicons: {
           appName: dotenv.parsed.SYSTEM_NAME,
           appDescription: dotenv.parsed.SYSTEM_NAME,
-          theme_color: tailwindConf.theme.colors.primary.default,
+          theme_color: tailwindConf.theme.colors.primary.DEFAULT,
         },
       }),
     ],
