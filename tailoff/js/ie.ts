093b3129c48306ffc35e7696b26d5564947c6560
Karel-Jan Van Haute <karel-jan@statik.be>
2021-10-07 16:55:36 +0200

    Cookie banner plugin

--- tailoff/js/ie.ts
+++ tailoff/js/ie.ts
@@ -1,7 +1,4 @@
 import '../css/ie.css';
 
-import { CookieComponent } from './ie/cookie.component';
-new CookieComponent();
-
 import { IEBannerComponent } from './ie/ieBanner.component';
 new IEBannerComponent();
