3854dccc15992b7ef62c270031f549f5cb8a192c
Karel-Jan Van Haute <karel-jan@statik.be>
2021-12-03 16:10:36 +0100

    Load more component update with infinite scroll and pagination.loaded event

--- tailoff/js/components/loadmore.component.ts
+++ tailoff/js/components/loadmore.component.ts
@@ -1,17 +1,16 @@
+import { DOMHelper } from '../utils/domHelper';
+
 export class LoadMoreComponent {
   private xhr: XMLHttpRequest;
+  private infiniteScroll = false;
 
   constructor() {
     document.addEventListener(
-      "click",
+      'click',
       (e) => {
         // loop parent nodes from the target to the delegation node
-        for (
-          let target = <Element>e.target;
-          target && !target.isSameNode(document);
-          target = target.parentElement
-        ) {
-          if (target.matches(".js-load-more")) {
+        for (let target = <Element>e.target; target && !target.isSameNode(document); target = target.parentElement) {
+          if (target.matches('.js-load-more')) {
             e.preventDefault();
             this.initAction(target);
             break;
@@ -20,77 +19,97 @@ export class LoadMoreComponent {
       },
       false
     );
-  }
 
-  private initAction(el: Element) {
-    el.classList.add("hidden");
-    const wrapper = el.getAttribute("data-load-wrapper");
-    if (wrapper) {
-      this.getNextPage(el.getAttribute("href"), wrapper);
-      document
-        .querySelector(`#${wrapper} .js-pagination-loader`)
-        .classList.remove("hidden");
-    } else {
-      this.getNextPage(el.getAttribute("href"));
-      document
-        .querySelector(".js-pagination-loader")
-        .classList.remove("hidden");
+    if (this.infiniteScroll || document.querySelector('.js-infinite-scroll')) {
+      this.initInfiniteScroll();
+
+      DOMHelper.onDynamicContent(document.documentElement, '.js-pagination', () => {
+        this.initInfiniteScroll();
+      });
     }
   }
 
-  private getNextPage(url, wrapper: string = "") {
+  private initInfiniteScroll() {
+    Array.from(document.querySelectorAll('.js-pagination.js-infinite-scroll')).forEach((el) => {
+      const observer = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              const loadMoreButton = entry.target.querySelector('.js-load-more');
+              if (loadMoreButton) {
+                this.initAction(loadMoreButton);
+              }
+            }
+          });
+        },
+        {
+          rootMargin: '10% 0% 10% 0%',
+          threshold: [0, 0.5, 1],
+        }
+      );
+      observer.observe(el);
+    });
+  }
+
+  private initAction(el: Element) {
+    el.classList.add('hidden');
+    const wrapper = el.getAttribute('data-load-wrapper');
+    if (wrapper) {
+      this.getNextPage(el.getAttribute('href'), wrapper);
+      document.querySelector(`#${wrapper} .js-pagination-loader`).classList.remove('hidden');
+    } else {
+      this.getNextPage(el.getAttribute('href'));
+      document.querySelector('.js-pagination-loader').classList.remove('hidden');
+    }
+  }
+
+  private getNextPage(url, wrapper: string = '') {
     const _self = this;
     if (this.xhr) {
       this.xhr.abort();
     }
 
     this.xhr = new XMLHttpRequest();
-    this.xhr.open("GET", url, true);
+    this.xhr.open('GET', url, true);
 
     this.xhr.onload = function () {
       if (this.status >= 200 && this.status < 400) {
-        const responseElement = document.implementation.createHTMLDocument("");
+        const responseElement = document.implementation.createHTMLDocument('');
         responseElement.body.innerHTML = this.response;
+        const children = responseElement.querySelector(`#${wrapper} .js-pagination-container`).children;
+        let container = document.querySelector('.js-pagination-container');
 
-        if (wrapper !== "") {
-          document.querySelector(
-            `#${wrapper} .js-pagination`
-          ).innerHTML = responseElement.querySelector(
+        if (wrapper !== '') {
+          document.querySelector(`#${wrapper} .js-pagination`).innerHTML = responseElement.querySelector(
             `#${wrapper} .js-pagination`
           ).innerHTML;
 
-          document
-            .querySelector(`#${wrapper} .js-pagination-container`)
-            .insertAdjacentHTML(
-              "beforeend",
-              responseElement.querySelector(
-                `#${wrapper} .js-pagination-container`
-              ).innerHTML
-            );
+          container = document.querySelector(`#${wrapper} .js-pagination-container`);
         } else {
-          document.querySelector(
-            ".js-pagination"
-          ).innerHTML = responseElement.querySelector(
-            ".js-pagination"
-          ).innerHTML;
-
-          document
-            .querySelector(".js-pagination-container")
-            .insertAdjacentHTML(
-              "beforeend",
-              responseElement.querySelector(".js-pagination-container")
-                .innerHTML
-            );
+          document.querySelector('.js-pagination').innerHTML =
+            responseElement.querySelector('.js-pagination').innerHTML;
         }
+
+        const elements = Array.from(children);
+
+        Array.from(children).forEach((child) => {
+          container.appendChild(child);
+        });
+
+        document.querySelector('.js-pagination-container').dispatchEvent(
+          new CustomEvent('pagination.loaded', {
+            detail: { elements: elements },
+          })
+        );
 
         // history.pushState("", "New URL: " + url, url);
       } else {
-        console.log("Something went wrong when fetching data");
+        console.log('Something went wrong when fetching data');
       }
     };
 
     this.xhr.onerror = function () {
-      console.log("There was a connection error");
+      console.log('There was a connection error');
     };
 
     this.xhr.send();
