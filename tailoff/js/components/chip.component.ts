25e95a31a267607507f698b27dbfc2097141c351
Karel-Jan Van Haute <karel-jan@statik.be>
2022-04-08 09:59:09 +0200

    Chip component

--- tailoff/js/components/chip.component.ts
+++ tailoff/js/components/chip.component.ts
@@ -2,211 +2,334 @@ import { A11yUtils } from '../utils/a11y';
 import { SiteLang } from '../utils/site-lang';
 
 export class ChipComponent {
-    constructor() {
-        const chips = Array.from(document.querySelectorAll('[data-s-chip]'));
-        chips.forEach((chip, index) => {
-            new ChipElement(chip as HTMLElement, index);
-        });
-    }
+  constructor() {
+    const chips = Array.from(document.querySelectorAll('[data-s-chip]'));
+    chips.forEach((chip, index) => {
+      new ChipElement(chip as HTMLElement, index);
+    });
+  }
 }
 
 class ChipElement {
-    private lang = require(`../i18n/s-chip-${SiteLang.getLang()}.json`);
-    private element: HTMLElement;
-    private triggerElement: HTMLButtonElement;
-    private triggerTextElement: HTMLElement;
-    private triggerClearElement: HTMLButtonElement;
-    private modalElement: HTMLElement;
-    private modalCloseElement: HTMLButtonElement;
-    private modalClearElement: HTMLButtonElement;
-    private toggleListener;
-    private changeListener;
-    private clearListener;
-    private escapeListener;
-    private clickOutsideListener;
-    private name: String;
-    private selected: String;
-    private index: Number;
+  private lang = require(`../i18n/s-chip-${SiteLang.getLang()}.json`);
+  private element: HTMLElement;
+  private triggerWrapperElement: HTMLDivElement;
+  private triggerElement: HTMLButtonElement;
+  private triggerTextElement: HTMLElement;
+  private triggerClearElement: HTMLButtonElement;
+  private modalElement: HTMLElement;
+  private modalCloseElement: HTMLButtonElement;
+  private modalClearElement: HTMLButtonElement;
+  private bubbleElement: HTMLDivElement;
+  private toggleListener;
+  private changeListener;
+  private clearListener;
+  private escapeListener;
+  private clickOutsideListener;
+  private name: string;
+  private selected: string;
+  private index: Number;
+  private showClearInButton: boolean;
+  private showClearInModal: boolean;
+  private showCloseButton: boolean;
+  private showBubble: boolean;
+  private closeOnChange: boolean;
 
-    constructor(element: HTMLElement, index) {
-        this.element = element;
-        this.index = index;
-        this.triggerElement = element.querySelector('.js-chip-trigger');
-        this.triggerTextElement = element.querySelector('.js-chip-trigger-text');
-        this.triggerClearElement = element.querySelector('.js-chip-trigger-clear');
-        this.modalElement = element.querySelector('.js-chip-modal');
-        this.modalCloseElement = element.querySelector('.js-chip-modal-close');
-        this.modalClearElement = element.querySelector('.js-chip-modal-clear');
+  constructor(element: HTMLElement, index) {
+    this.element = element;
+    this.index = index;
 
-        this.toggleListener = this.toggleAction.bind(this);
-        this.changeListener = this.changeAction.bind(this);
-        this.clearListener = this.clearAction.bind(this);
-        this.escapeListener = this.escapeAction.bind(this);
-        this.clickOutsideListener = this.clickOutsideAction.bind(this);
+    this.showClearInButton = this.element.hasAttribute('data-s-chip-show-clear-in-button')
+      ? this.element.getAttribute('data-s-chip-show-clear-in-button') === 'true'
+      : true;
+    this.showClearInModal = this.element.hasAttribute('data-s-chip-show-clear-in-modal')
+      ? this.element.getAttribute('data-s-chip-show-clear-in-modal') === 'true'
+      : true;
+    this.showCloseButton = this.element.hasAttribute('data-s-chip-show-close-button')
+      ? this.element.getAttribute('data-s-chip-show-close-button') === 'true'
+      : true;
+    this.showBubble = this.element.hasAttribute('data-s-chip-show-bubble')
+      ? this.element.getAttribute('data-s-chip-show-bubble') === 'true'
+      : true;
+    this.closeOnChange = this.element.hasAttribute('data-s-chip-close-on-change')
+      ? this.element.getAttribute('data-s-chip-close-on-change') === 'true'
+      : true;
 
-        this.name = element.getAttribute('data-s-chip');
+    this.toggleListener = this.toggleAction.bind(this);
+    this.changeListener = this.changeAction.bind(this);
+    this.clearListener = this.clearAction.bind(this);
+    this.escapeListener = this.escapeAction.bind(this);
+    this.clickOutsideListener = this.clickOutsideAction.bind(this);
 
-        this.initInputs();
+    this.name = element.getAttribute('data-s-chip');
+
+    this.initComponents();
+    this.initInputs();
+    this.selected = this.getSelected();
+    this.initTrigger();
+    this.initModal();
+
+    if (this.showBubble) {
+      this.setBubbleCount();
+    }
+  }
+
+  private initComponents() {
+    this.modalElement = document.createElement('div');
+    while (this.element.childNodes.length > 0) {
+      this.modalElement.appendChild(this.element.childNodes[0]);
+    }
+    this.modalElement.classList.add('hidden');
+    this.modalElement.classList.add('chip__modal');
+    this.element.insertAdjacentElement('afterbegin', this.modalElement);
+
+    this.triggerWrapperElement = document.createElement('div');
+    this.triggerWrapperElement.classList.add('chip__trigger-wrapper');
+
+    this.triggerElement = document.createElement('button');
+    this.triggerElement.classList.add('chip__trigger');
+    this.triggerTextElement = document.createElement('span');
+    this.triggerElement.insertAdjacentElement('beforeend', this.triggerTextElement);
+
+    this.triggerWrapperElement.insertAdjacentElement('beforeend', this.triggerElement);
+
+    if (this.showClearInButton) {
+      this.triggerClearElement = document.createElement('button');
+      this.triggerClearElement.classList.add('hidden');
+      this.triggerClearElement.classList.add('chip__trigger-clear');
+      this.triggerClearElement.ariaLabel = this.lang.clear;
+      const clearLabel = document.createElement('span');
+      clearLabel.innerHTML = this.lang.clear;
+      this.triggerClearElement.insertAdjacentElement('beforeend', clearLabel);
+      this.triggerWrapperElement.insertAdjacentElement('beforeend', this.triggerClearElement);
+    }
+
+    if (this.showBubble) {
+      this.bubbleElement = document.createElement('div');
+      this.bubbleElement.classList.add('chip__bubble');
+      this.bubbleElement.classList.add('hidden');
+      this.triggerWrapperElement.insertAdjacentElement('beforeend', this.bubbleElement);
+    }
+
+    this.element.insertAdjacentElement('afterbegin', this.triggerWrapperElement);
+
+    if (this.showCloseButton) {
+      this.modalCloseElement = document.createElement('button');
+      this.modalCloseElement.classList.add('chip__modal-close');
+      this.modalCloseElement.ariaLabel = this.lang.closeModal;
+      const closeLabel = document.createElement('span');
+      closeLabel.innerHTML = this.lang.modalClose;
+      this.modalCloseElement.insertAdjacentElement('beforeend', closeLabel);
+      this.modalElement.insertAdjacentElement('afterbegin', this.modalCloseElement);
+    }
+
+    if (this.showClearInModal) {
+      this.modalClearElement = document.createElement('button');
+      this.modalClearElement.classList.add('chip__modal-clear');
+      this.modalClearElement.ariaLabel = this.lang.modalClear;
+      if (this.element.hasAttribute('data-s-chip-modal-clear-label')) {
+        this.modalClearElement.innerText = this.element.getAttribute('data-s-chip-modal-clear-label');
+      }
+      this.modalElement.insertAdjacentElement('beforeend', this.modalClearElement);
+    }
+  }
+
+  private initTrigger() {
+    this.triggerElement.setAttribute('aria-haspopup', 'dialog');
+    if (this.showBubble) {
+      this.triggerTextElement.innerText = this.name;
+      this.triggerElement.setAttribute('aria-label', `${this.name}, ${this.lang.triggerNotSelectedLabel}`);
+    } else {
+      this.setTriggerLabel();
+      this.setTriggerText();
+    }
+    this.setTriggerState();
+    this.triggerElement.addEventListener('click', this.toggleListener);
+    if (this.showClearInButton) {
+      this.triggerClearElement.addEventListener('click', this.clearListener);
+    }
+  }
+
+  private initModal() {
+    this.modalElement.setAttribute('aria-modal', 'true');
+    this.modalElement.setAttribute('role', 'dialog');
+    const heading = this.modalElement.querySelector('h1,h2');
+    const id = `chip-${this.index}-heading`;
+    heading.id = id;
+    this.modalElement.setAttribute('aria-labelledby', id);
+    this.modalElement.setAttribute('tabindex', '-1');
+  }
+
+  private initInputs() {
+    const inputs = this.modalElement.querySelectorAll('input');
+    Array.from(inputs).forEach((input) => {
+      const labelElement = this.modalElement.querySelector(`label[for=${input.id}]`);
+      if (labelElement) {
+        input.setAttribute('data-s-chip-input-label', labelElement.innerHTML.trim());
+      }
+    });
+  }
+
+  private toggleModal() {
+    if (this.modalElement.classList.contains('hidden')) {
+      this.modalElement.classList.remove('hidden');
+      this.trapFocus();
+      this.modalElement.addEventListener('change', this.changeListener);
+      if (this.showCloseButton) this.modalCloseElement.addEventListener('click', this.toggleListener);
+      if (this.showClearInModal) this.modalClearElement.addEventListener('click', this.clearListener);
+
+      document.addEventListener('click', this.clickOutsideListener);
+      document.addEventListener('keydown', this.escapeListener);
+    } else {
+      this.modalElement.classList.add('hidden');
+      this.modalElement.removeEventListener('change', this.changeListener);
+      if (this.showCloseButton) this.modalCloseElement.removeEventListener('click', this.toggleListener);
+      if (this.showClearInModal) this.modalClearElement.removeEventListener('click', this.clearListener);
+
+      document.removeEventListener('click', this.clickOutsideListener);
+      document.removeEventListener('keydown', this.escapeListener);
+    }
+  }
+
+  private trapFocus() {
+    A11yUtils.keepFocus(this.modalElement);
+    this.modalElement.focus();
+  }
+
+  private getTriggerText() {
+    const count = this.getCount();
+    switch (count) {
+      case 0:
+        return this.name;
+      case 1:
+        return this.selected;
+      default:
+        return `${this.selected} +${count - 1}`;
+    }
+  }
+
+  private getTriggerLabel() {
+    const count = this.getCount();
+    switch (count) {
+      case 0:
+        return `${this.name}, ${this.lang.triggerNotSelectedLabel}`;
+      case 1:
+        return `${this.selected}, ${this.name}, ${this.lang.triggerSelectedLabel}`;
+      default:
+        return `${this.selected} +${count - 1}, ${this.name}, ${this.lang.triggerSelectedLabel}`;
+    }
+  }
+
+  private setTriggerText() {
+    const text = this.getTriggerText();
+    this.triggerTextElement.innerText = text;
+  }
+
+  private setTriggerLabel() {
+    const label = this.getTriggerLabel();
+    this.triggerElement.setAttribute('aria-label', label);
+  }
+
+  private setBubbleCount() {
+    const count = this.getCount();
+    if (count > 0) {
+      this.bubbleElement.innerText = `${count}`;
+      this.bubbleElement.classList.remove('hidden');
+      this.bubbleElement.classList.add('flex');
+    } else {
+      this.bubbleElement.classList.add('hidden');
+      this.bubbleElement.classList.remove('flex');
+    }
+  }
+
+  private getSelected() {
+    const checked = this.modalElement.querySelector('input:checked');
+    return checked ? checked.getAttribute('data-s-chip-input-label') : '';
+  }
+
+  private getCount() {
+    const checked = this.modalElement.querySelectorAll('input:checked');
+    return checked.length;
+  }
+
+  private setTriggerState() {
+    if (this.selected) {
+      this.element.classList.add('active');
+      if (this.showClearInButton) {
+        this.triggerClearElement.classList.remove('hidden');
+      }
+    } else {
+      this.element.classList.remove('active');
+      if (this.showClearInButton) {
+        this.triggerClearElement.classList.add('hidden');
+      }
+    }
+  }
+
+  private toggleAction() {
+    this.toggleModal();
+  }
+
+  private changeAction(event: Event) {
+    const element = event.target as HTMLInputElement;
+    const label = element.getAttribute('data-s-chip-input-label');
+
+    if (element.checked) {
+      if (element.getAttribute('type') === 'checkbox') {
+        if (!this.selected) {
+          this.selected = label;
+        }
+      } else {
+        this.selected = label;
+      }
+    } else {
+      if (this.selected === label) {
         this.selected = this.getSelected();
-        this.initTrigger();
-        this.initModal();
+      }
     }
 
-    private initTrigger() {
-        this.triggerElement.setAttribute('aria-haspopup', 'true');
-        this.setTriggerLabel();
-        this.setTriggerText();
-        this.setTriggerState();
-        this.triggerElement.addEventListener('click', this.toggleListener);
-        this.triggerClearElement.addEventListener('click', this.clearListener);
+    if (this.showBubble) {
+      this.setBubbleCount();
+    } else {
+      this.setTriggerText();
+      this.setTriggerLabel();
     }
+    this.setTriggerState();
 
-    private initModal() {
-        this.modalElement.setAttribute('aria-modal', 'true');
-        this.modalElement.setAttribute('role', 'dialog');
-        const heading = this.modalElement.querySelector('h1,h2');
-        const id = `chip-${this.index}-heading`;
-        heading.id = id;
-        this.modalElement.setAttribute('aria-labelledby', id);
-        this.modalElement.setAttribute('tabindex', '-1');
+    if (this.closeOnChange) {
+      this.toggleModal();
     }
+  }
 
-    private initInputs() {
-        const inputs = this.modalElement.querySelectorAll('input');
-        Array.from(inputs).forEach(input => {
-            const labelElement = this.modalElement.querySelector(`label[for=${input.id}]`);
-            input.setAttribute('data-s-chip-input-label', labelElement.textContent.trim());
-        })
+  private clearAction(event) {
+    const checkedInputs = this.modalElement.querySelectorAll('input:checked');
+    Array.from(checkedInputs).forEach((input: HTMLInputElement) => {
+      input.checked = false;
+    });
+    this.selected = '';
+    const changeEvent = new Event('jschange', { bubbles: true });
+    const input = this.modalElement.querySelector('input');
+    input.dispatchEvent(changeEvent);
+
+    if (this.showBubble) {
+      this.setBubbleCount();
+    } else {
+      this.setTriggerText();
+      this.setTriggerLabel();
     }
+    this.setTriggerState();
+  }
 
-    private toggleModal() {
-        if (this.modalElement.classList.contains('hidden')) {
-            this.modalElement.classList.remove('hidden');
-            this.trapFocus();
-            this.modalCloseElement.addEventListener('click', this.toggleListener);
-            this.modalElement.addEventListener('change', this.changeListener);
-            this.modalClearElement.addEventListener('click', this.clearListener);
-
-            document.addEventListener('click', this.clickOutsideListener);
-            document.addEventListener('keydown', this.escapeListener);
-        } else {
-            this.modalElement.classList.add('hidden');
-            this.modalCloseElement.removeEventListener('click', this.toggleListener);
-            this.modalElement.removeEventListener('change', this.changeListener);
-            this.modalClearElement.removeEventListener('click', this.clearListener);
-
-            document.removeEventListener('click', this.clickOutsideListener);
-            document.removeEventListener('keydown', this.escapeListener);
-        }
+  private escapeAction(event) {
+    if (event.keyCode === 27 || event.key === 'Escape') {
+      this.toggleModal();
     }
+  }
 
-    private trapFocus() {
-        A11yUtils.keepFocus(this.modalElement);
-        this.modalElement.focus();
+  private clickOutsideAction(event) {
+    if (this.element.contains(event.target)) {
+      return;
     }
-
-    private getTriggerText() {
-        const count = this.getCount();
-        switch (count) {
-            case 0:
-                return this.name;
-            case 1:
-                return this.selected;
-            default:
-                return `${this.selected} +${count - 1}`;
-        }
-    }
-
-    private getTriggerLabel() {
-        const count = this.getCount();
-        switch (count) {
-            case 0:
-                return `${this.name}, ${this.lang.triggerNotSelectedLabel}`;
-            case 1:
-                return `${this.selected}, ${this.name}, ${this.lang.triggerSelectedLabel}`;
-            default:
-                return `${this.selected} +${count - 1}, ${this.name}, ${this.lang.triggerSelectedLabel}`;
-        }
-    }
-
-    private setTriggerText() {
-        const text = this.getTriggerText();
-        this.triggerTextElement.textContent = text;
-    }
-
-    private setTriggerLabel() {
-        const label = this.getTriggerLabel();
-        this.triggerElement.setAttribute('aria-label', label);
-    }
-
-    private getSelected() {
-        const checked = this.modalElement.querySelector('input:checked');
-        return checked ? checked.getAttribute('data-s-chip-input-label') : '';
-    }
-
-    private getCount() {
-        const checked = this.modalElement.querySelectorAll('input:checked');
-        return checked.length;
-    }
-
-    private setTriggerState() {
-        if (this.selected) {
-            this.element.classList.add('active')
-        } else {
-            this.element.classList.remove('active');
-        }
-    }
-
-    private toggleAction() {
-        this.toggleModal();
-    }
-
-    private changeAction(event: Event) {
-        const label = event.target.getAttribute('data-s-chip-input-label');
-
-        if (event.target.checked) {
-            if(event.target.getAttribute('type') === 'checkbox') {
-                if (!this.selected) {
-                    this.selected = label;
-                }
-            } else {
-                this.selected = label;
-            }
-        } else {
-            if (this.selected === label) {
-                this.selected = this.getSelected();
-            }
-        }
-
-        const checkedInputs = this.modalElement.querySelectorAll('input:checked');
-        const count = checkedInputs.length;
-
-        this.setTriggerText();
-        this.setTriggerLabel();
-        this.setTriggerState();
-    }
-
-    private clearAction(event) {
-        const checkedInputs = this.modalElement.querySelectorAll('input:checked');
-        Array.from(checkedInputs).forEach(input => {
-            input.checked = false;
-        })
-        this.selected = '';
-        const changeEvent = new Event('change', { bubbles: true });
-        const input = this.modalElement.querySelector('input');
-        input.dispatchEvent(changeEvent);
-    }
-
-    private escapeAction(event) {
-        if (event.keyCode === 27 || event.key === 'Escape') {
-            this.toggleModal();
-        }
-    }
-
-    private clickOutsideAction(event) {
-        if (this.element.contains(event.target)) {
-            return;
-        }
-        this.toggleModal();
-    }
+    this.toggleModal();
+  }
 }
