8771a279af48e9e59aab0d94089ba23899b636f9
Karel-Jan Van Haute <karel-jan@statik.be>
2022-09-15 10:24:17 +0200

    onDynamicContent extended to work with attributes

--- tailoff/js/utils/domHelper.ts
+++ tailoff/js/utils/domHelper.ts
@@ -2,12 +2,12 @@ export class DOMHelper {
   constructor() {}
 
   public static loadScript(url, cb) {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
     script.src = url;
 
-    if (typeof cb !== "undefined") {
-      script.addEventListener("load", function () {
+    if (typeof cb !== 'undefined') {
+      script.addEventListener('load', function () {
         cb();
       });
     }
@@ -18,30 +18,44 @@ export class DOMHelper {
   public static onDynamicContent(
     parent: Element,
     selector: string,
-    callback: Function
+    callback: Function,
+    includeAttributes: boolean | string = false
   ) {
-    const mutationObserver: MutationObserver = new MutationObserver(
-      (mutationsList) => {
-        for (let mutation of mutationsList) {
-          if (mutation.type === "childList") {
-            Array.from(mutation.addedNodes).forEach((node: HTMLElement) => {
-              if (node.nodeType == 1) {
-                const results = node.querySelectorAll(selector);
-                if (results.length > 0) {
-                  callback(results);
-                } else {
-                  if (node.matches(selector)) {
-                    callback([node]);
-                  }
+    const mutationObserver: MutationObserver = new MutationObserver((mutationsList) => {
+      for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          Array.from(mutation.addedNodes).forEach((node: HTMLElement) => {
+            if (node.nodeType == 1) {
+              const results = node.querySelectorAll(selector);
+              if (results.length > 0) {
+                callback(results);
+              } else {
+                if (node.matches(selector)) {
+                  callback([node]);
                 }
               }
-            });
+            }
+          });
+        }
+        if (mutation.type === 'attributes' && includeAttributes) {
+          if (typeof includeAttributes == 'string') {
+            if (mutation.attributeName == includeAttributes) {
+              const results = (mutation.target as HTMLElement).matches(selector);
+              if (results) {
+                callback([mutation.target]);
+              }
+            }
+          } else {
+            const results = (mutation.target as HTMLElement).matches(selector);
+            if (results) {
+              callback([mutation.target]);
+            }
           }
         }
       }
-    );
+    });
     mutationObserver.observe(parent, {
-      attributes: false,
+      attributes: typeof includeAttributes == 'boolean' ? includeAttributes : includeAttributes.length > 0,
       childList: true,
       subtree: true,
     });
