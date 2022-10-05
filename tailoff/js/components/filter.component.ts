0eed90e4fc3c1aab2f57658fc706a91d256efab0
Karel-Jan Van Haute <karel-jan@statik.be>
2022-10-05 17:19:28 +0200

    Clear filter only appears when needed

--- tailoff/js/components/filter.component.ts
+++ tailoff/js/components/filter.component.ts
@@ -191,6 +191,25 @@ export class FilterComponent {
       this.showLoading();
       this.getFilterData(window.location.origin + window.location.pathname, true);
     });
+    this.checkClearButtonStatus();
+  }
+
+  private checkClearButtonStatus() {
+    const formData = new FormData(this.formElement);
+    let isEmpty = true;
+    formData.forEach((value, key) => {
+      if (value !== '' && value !== undefined) {
+        isEmpty = false;
+      }
+    });
+
+    if (isEmpty) {
+      if (!this.clearFilterButtonElement.hasAttribute('data-s-always-show')) {
+        this.clearFilterButtonElement.classList.add('hidden');
+      }
+    } else {
+      this.clearFilterButtonElement.classList.remove('hidden');
+    }
   }
 
   private initShowMore() {
@@ -279,6 +298,8 @@ export class FilterComponent {
         if (extraInfoBlock) {
           _self.extraInfoElement.innerHTML = extraInfoBlock.innerHTML;
         }
+
+        _self.checkClearButtonStatus();
       } else {
         console.error('Something went wrong when fetching data.');
       }
@@ -375,6 +396,7 @@ export class FilterComponent {
     const filterElementsCleared = document.createEvent('HTMLEvents');
     filterElementsCleared.initEvent('filterElementsCleared', false, true);
     document.dispatchEvent(filterElementsCleared);
+    this.checkClearButtonStatus();
   }
 
   private styleClear() {
@@ -471,5 +493,6 @@ export class FilterComponent {
     });
 
     this.styleClear();
+    this.checkClearButtonStatus();
   }
 }
