98c2a70045d317c95545dbc4dfcd1706d0cc6976
Karel-Jan Van Haute <karel-jan@statik.be>
2023-09-29 08:53:28 +0200

    some layout improvements

--- config/config-values-field.php
+++ config/config-values-field.php
@@ -9,11 +9,14 @@ return [
             'section--primary' => 'Primary',
         ],
         'CTA styles' => [
-            'btn' => 'Primary',
+            'btn btn--primary' => 'Primary',
+            'btn btn--primary btn--ext' => 'Primary >',
             'btn btn--secondary' => 'Secondary',
+            'btn btn--secondary btn--ext' => 'Secondary >',
             'btn btn--ghost' => 'Ghost',
-            'link link--ext' => 'Link >',
+            'btn btn--ghost btn--ext' => 'Ghost >',
             'link' => 'Link',
+            'link link--ext' => 'Link >',
         ],
     ]
 ];
