2c6ce5e6a9130c27cd4a386dcc0a419abb1b5999
Karel-Jan Van Haute <karel-jan@statik.be>
2024-02-01 20:18:50 +0100

    Make loader files for all js components Fixes #181

--- tailoff/js/site.ts
+++ tailoff/js/site.ts
@@ -7,120 +7,225 @@ if (import.meta.hot) {
   });
 }
 
-/**
- * Essential components
- */
 import { ComponentLoader } from './loader/component.loader';
 const componentLoader = new ComponentLoader();
 
-componentLoader.loadComponent('general', 'GeneralComponent', 'body');
-
-componentLoader.loadComponent('responsiveBackground', 'ResponsiveBackgroundComponent', '.js-bg-src');
-
-/**
- * Extra components
- */
-componentLoader.loadComponent('ajaxPaging', 'AjaxPagingComponent', '.js-ajax-paging');
-
-componentLoader.loadComponent(
-  'ajaxSearch',
-  'AjaxSearchComponent',
-  '[data-s-ajax-search], [data-s-ajax-search-callback]'
-);
-
-componentLoader.loadComponent('autocomplete', 'AutocompleteComponent', '[data-s-autocomplete]');
-
-componentLoader.loadComponent('chip', 'ChipComponent', '[data-s-chip]');
-
-componentLoader.loadComponent('datepicker', 'DatePickerComponent', '.js-date-picker');
-
-componentLoader.loadComponent('dropdown', 'DropdownComponent', '.js-dropdown');
-
-componentLoader.loadComponent('filter', 'FilterComponent', '.js-filter-form');
-
-componentLoader.loadComponent('flyout', 'FlyoutComponent', '#flyout');
-
-componentLoader.loadComponent('formie', 'FormieComponent', '.fui-form');
-
-componentLoader.loadComponent(
-  'formOptionalBlocks',
-  'FormOptionalBlocks',
-  '.js-form-optional-block, js-form-optional-required'
-);
-
-componentLoader.loadComponent('formOtherRadio', 'formOtherRadioComponent', '.js-other-radio');
-
-componentLoader.loadComponent('glide', 'GlideComponent', '.js-slider');
-
-componentLoader.loadComponent('googleMaps', 'GoogleMapsComponent', '.js-google-map');
-
-componentLoader.loadComponent('indeterminateChecks', 'IndeterminateChecksComponent', 'ul.js-indeterminate-checks');
-
-componentLoader.loadComponent('leaflet', 'LeafletComponent', '.leaflet-map');
-
-componentLoader.loadComponent('loadMore', 'LoadMoreComponent', '.js-load-more');
-
-componentLoader.loadComponent('masonry', 'MasonryComponent', '.js-masonry-item');
-
-componentLoader.loadComponent('matrix', 'MatrixComponent', '.js-matrix-add');
-
-componentLoader.loadComponent('modal', 'ModalComponent', '.js-modal', [
-  { path: 'modal/image.plugin', name: 'ImageModalPlugin' },
-  { path: 'modal/video.plugin', name: 'VideoModalPlugin' },
+const components = [
   {
-    path: 'modal/confirmation.plugin',
-    name: 'ConfirmationModalPlugin',
+    name: 'general',
+    className: 'GeneralComponent',
+    selector: 'body',
   },
-]);
-
-componentLoader.loadComponent('pageFind', 'PageFindComponent', 'form.js-find-form');
-
-componentLoader.loadComponent('passwordToggle', 'PasswordToggleComponent', '.js-password-toggle');
-
-componentLoader.loadComponent('pullOut', 'PullOutComponent', '.js-pull-out');
-
-componentLoader.loadComponent('rangeSlider', 'RangeSliderComponent', '.js-range-slider');
-
-componentLoader.loadComponent('scrollAnimation', 'ScrollAnimationComponent', '.scroll-ani');
-
-componentLoader.loadComponent('scrollParallax', 'ScrollParallaxComponent', '[data-s-parallax]');
-
-componentLoader.loadComponent(
-  'scrollToAnchor',
-  'ScrollToAnchorComponent',
-  'a.js-smooth-scroll, .js-smooth-scroll-attr'
-);
-
-componentLoader.loadComponent('search', 'SearchComponent', '.js-search-form');
-
-componentLoader.loadComponent('site', 'SiteComponent', 'body');
-
-componentLoader.loadComponent('stickyHeader', 'StickyHeader', '[data-s-sticky-header]');
-
-componentLoader.loadComponent('table', 'TableComponent', '.custom-table table');
-
-componentLoader.loadComponent('tabs', 'TabsComponent', 'ul.js-tabs');
-
-componentLoader.loadComponent('toggle', 'ToggleComponent', '[data-s-toggle]');
-
-componentLoader.loadComponent('tooltip', 'TooltipComponent', '[data-tippy-content], [data-tippy-template]');
-
-componentLoader.loadComponent('validation', 'ValidationComponent', '[data-s-validate]', [
-  { path: 'validation/countdown.plugin', name: 'CountdownPlugin' },
   {
-    path: 'validation/passwordConfirm.plugin',
-    name: 'PasswordConfirmPlugin',
+    name: 'responsiveBackground',
+    className: 'ResponsiveBackgroundComponent',
+    selector: '.js-bg-src, [data-bg-target]',
   },
-  { path: 'validation/checkboxRange.plugin', name: 'CheckboxRangePlugin' },
   {
-    path: 'validation/passwordStrength.plugin',
-    name: 'PasswordStrengthPlugin',
+    name: 'ajaxPaging',
+    className: 'AjaxPagingComponent',
+    selector: '.js-ajax-paging',
   },
-]);
+  {
+    name: 'ajaxSearch',
+    className: 'AjaxSearchComponent',
+    selector: '[data-s-ajax-search], [data-s-ajax-search-callback]',
+  },
+  {
+    name: 'autocomplete',
+    className: 'AutocompleteComponent',
+    selector: '[data-s-autocomplete]',
+  },
+  {
+    name: 'chip',
+    className: 'ChipComponent',
+    selector: '[data-s-chip]',
+  },
+  {
+    name: 'datepicker',
+    className: 'DatePickerComponent',
+    selector: '.js-date-picker',
+  },
+  {
+    name: 'dropdown',
+    className: 'DropdownComponent',
+    selector: '.js-dropdown',
+  },
+  {
+    name: 'filter',
+    className: 'FilterComponent',
+    selector: '.js-filter-form',
+  },
+  {
+    name: 'flyout',
+    className: 'FlyoutComponent',
+    selector: '#flyout',
+  },
+  {
+    name: 'formie',
+    className: 'FormieComponent',
+    selector: '.fui-form',
+  },
+  {
+    name: 'formOptionalBlocks',
+    className: 'FormOptionalBlocks',
+    selector: '.js-form-optional-block, js-form-optional-required',
+  },
+  {
+    name: 'formOtherRadio',
+    className: 'formOtherRadioComponent',
+    selector: '.js-other-radio',
+  },
+  {
+    name: 'glide',
+    className: 'GlideComponent',
+    selector: '.js-slider',
+  },
+  {
+    name: 'googleMaps',
+    className: 'GoogleMapsComponent',
+    selector: '.js-google-map',
+  },
+  {
+    name: 'indeterminateChecks',
+    className: 'IndeterminateChecksComponent',
+    selector: 'ul.js-indeterminate-checks',
+  },
+  {
+    name: 'leaflet',
+    className: 'LeafletComponent',
+    selector: '.leaflet-map',
+  },
+  {
+    name: 'loadMore',
+    className: 'LoadMoreComponent',
+    selector: '.js-load-more',
+  },
+  {
+    name: 'masonry',
+    className: 'MasonryComponent',
+    selector: '.js-masonry-item',
+  },
+  {
+    name: 'matrix',
+    className: 'MatrixComponent',
+    selector: '.js-matrix-add',
+  },
+  {
+    name: 'modal',
+    className: 'ModalComponent',
+    selector: '.js-modal',
+    plugins: [
+      { path: 'modal/image.plugin', name: 'ImageModalPlugin' },
+      { path: 'modal/video.plugin', name: 'VideoModalPlugin' },
+      {
+        path: 'modal/confirmation.plugin',
+        name: 'ConfirmationModalPlugin',
+      },
+    ],
+  },
+  {
+    name: 'pageFind',
+    className: 'PageFindComponent',
+    selector: 'form.js-find-form',
+  },
+  {
+    name: 'passwordToggle',
+    className: 'PasswordToggleComponent',
+    selector: '.js-password-toggle',
+  },
+  {
+    name: 'pullOut',
+    className: 'PullOutComponent',
+    selector: '.js-pull-out',
+  },
+  {
+    name: 'rangeSlider',
+    className: 'RangeSliderComponent',
+    selector: '.js-range-slider',
+  },
+  {
+    name: 'scrollAnimation',
+    className: 'ScrollAnimationComponent',
+    selector: '.scroll-ani',
+  },
+  {
+    name: 'scrollParallax',
+    className: 'ScrollParallaxComponent',
+    selector: '[data-s-parallax]',
+  },
+  {
+    name: 'scrollToAnchor',
+    className: 'ScrollToAnchorComponent',
+    selector: 'a.js-smooth-scroll, .js-smooth-scroll-attr',
+  },
+  {
+    name: 'search',
+    className: 'SearchComponent',
+    selector: '.js-search-form',
+  },
+  {
+    name: 'site',
+    className: 'SiteComponent',
+    selector: 'body',
+  },
+  {
+    name: 'stickyHeader',
+    className: 'StickyHeader',
+    selector: '[data-s-sticky-header]',
+  },
+  {
+    name: 'table',
+    className: 'TableComponent',
+    selector: '.custom-table table',
+  },
+  {
+    name: 'tabs',
+    className: 'TabsComponent',
+    selector: 'ul.js-tabs',
+  },
+  {
+    name: 'toggle',
+    className: 'ToggleComponent',
+    selector: '[data-s-toggle]',
+  },
+  {
+    name: 'tooltip',
+    className: 'TooltipComponent',
+    selector: '[data-tippy-content], [data-tippy-template]',
+  },
+  {
+    name: 'validation',
+    className: 'ValidationComponent',
+    selector: '[data-s-validate]',
+    plugins: [
+      { path: 'validation/countdown.plugin', name: 'CountdownPlugin' },
+      {
+        path: 'validation/passwordConfirm.plugin',
+        name: 'PasswordConfirmPlugin',
+      },
+      { path: 'validation/checkboxRange.plugin', name: 'CheckboxRangePlugin' },
+      {
+        path: 'validation/passwordStrength.plugin',
+        name: 'PasswordStrengthPlugin',
+      },
+    ],
+  },
+  {
+    name: 'videoBackground',
+    className: 'VideoBackgroundComponent',
+    selector: '.js-video-bg, .js-video-container',
+  },
+  {
+    name: 'videoToggle',
+    className: 'VideoToggleComponent',
+    selector: 'button[data-s-video-toggle]',
+  },
+];
 
-componentLoader.loadComponent('videoBackground', 'VideoBackgroundComponent', '.js-video-bg, .js-video-container');
-
-componentLoader.loadComponent('videoToggle', 'VideoToggleComponent', 'button[data-s-video-toggle]');
+components.forEach((component) => {
+  componentLoader.loadComponent(component.name, component.className, component.selector, component.plugins ?? []);
+});
 
 /**
  * CSS import
