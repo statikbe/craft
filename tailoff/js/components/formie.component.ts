3e3c8e6497083e69c0ec9406734cae1a9994eda0
Karel-Jan Van Haute <karel-jan@statik.be>
2024-02-01 10:06:15 +0100

    Add multiselect to formie component Fixes #246

--- tailoff/js/components/formie.component.ts
+++ tailoff/js/components/formie.component.ts
@@ -1,5 +1,5 @@
-import { SiteLang } from '../utils/site-lang';
-import { ArrayPrototypes } from '../utils/prototypes/array.prototypes';
+import { SiteLang } from "../utils/site-lang";
+import { ArrayPrototypes } from "../utils/prototypes/array.prototypes";
 
 ArrayPrototypes.activateFrom();
 
@@ -12,14 +12,26 @@ declare global {
 export class FormieComponent {
   private siteLang = SiteLang.getLang();
   // public lang = require(`../i18n/formie-${this.siteLang}.json`);
-  public lang = import(`../i18n/formie-${this.siteLang}.json`).then((module) => module.default);
+  public lang = import(`../i18n/formie-${this.siteLang}.json`).then(
+    (module) => module.default
+  );
 
   constructor() {
     window.FormieTranslations = this.lang;
 
-    Array.from(document.querySelectorAll('select.fui-select')).forEach((element) => {
-      const container = element.closest('.fui-input-container');
-      container.classList.add('fui-select-container');
+    Array.from(document.querySelectorAll("select.fui-select")).forEach(
+      (element) => {
+        const container = element.closest(".fui-input-container");
+        container.classList.add("fui-select-container");
+      }
+    );
+
+    Array.from(
+      document.querySelectorAll("select.fui-select[multiple]")
+    ).forEach((element) => {
+      setTimeout(() => {
+        element.setAttribute("data-s-autocomplete", "");
+      }, 0);
     });
   }
 }
