ebf7924ebf7b90815211593a281ffe445e8a4571
Karel-Jan Van Haute <karel-jan@statik.be>
2025-04-02 10:00:40 +0200

    remove postcss

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-import postcssCustomProperties from 'postcss-custom-properties';
-import postcssMixins from 'postcss-mixins';
-
-export default {
-  plugins: [postcssCustomProperties, postcssMixins],
-};
